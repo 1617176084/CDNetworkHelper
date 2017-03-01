5eabcc2ddc39b9f6b4a9a7bf3c9805c2a8703813
your namemyWang <1617176084@qq.com>
2017-03-01 16:54:52 +0800

    no message

--- CDNetworkHelper/CDNetworkHelper/ViewController.m
+++ CDNetworkHelper/CDNetworkHelper/ViewController.m
@@ -6,8 +6,8 @@
 //  Copyright © 2017年 Apple. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "CDNetworkHelper.h"
+#import "ViewController.h"
 
 @interface ViewController ()
 
@@ -16,16 +16,14 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    [CDNetworkHelper POST:@"" parameters:NULL success:NULL failure:NULL];
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+  [CDNetworkHelper POST:@"" parameters:NULL success:NULL failure:NULL];
 }
-
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
-
 
 @end
